ecf8662f3b54463c0d49a8dfd49f4e86e2cf2fe1
alex kimathi <keemalekie@gmail.com>
2022-09-26 21:07:40 +0300

    commit tree e4dd7345b0eadb303dfd6e11fc0a4370eb8ad62a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,11 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+   
+  end
+  def students
+    students = Student.find(params[:id])
+    render json: students
   end
 
 end
